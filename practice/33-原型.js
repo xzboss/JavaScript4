276cfa21b91cafd15ab4fbe273fb994a696f0c84
徐洲 <xuzhou@changba.com>
2023-12-01 19:02:05 +0800

    feat: 🎸 something

--- practice/33-原型.js
+++ practice/33-原型.js
@@ -1,13 +1,77 @@
-class Person{
-    constructor(name){
-        this.name = name
-    }
-    sayName=()=>{
-        console.log(this.name)
-    }
+// // class Person{
+// //     constructor(name){
+// //         this.name = name
+// //     }
+// //     sayName=()=>{
+// //         console.log(this.name)
+// //     }
+// // }
+// // const person = new Person('111')
+// // const person2 = new Person('222')
+// // person.sayName()
+// // person2.sayName()
+// // console.log(person.sayName === person2.sayName,Person.sayName)
+
+// const obj = {a:1,[Symbol()]:'sy'}
+// obj.__proto__.fhg = '344'
+// Object.defineProperty(obj,'name',{
+//     enumerable:false,
+//     configurable:true,
+//     value:'xz',
+//     writable:true
+// })
+// for(const i in obj){
+//     console.log(i) 
+// }
+// console.log(Object.keys(obj))
+// console.log(Object.getOwnPropertyNames(obj))
+// console.log(Object.getOwnPropertySymbols(obj))
+
+
+
+// const obj = {}
+// Object.prototype.pro = {}
+// const a = {}
+// obj.pro.name = 'hell'
+// console.log(obj.pro)
+// console.log(Object.getOwnPropertyNames(Object.prototype))
+
+// function Person() {
+
+// }
+// const person = new Person()
+// console.log( person.prototype )
+
+// class Person {
+//     name() {
+//         return 'xz'
+//     }
+// }
+// const person = new Person()
+// Object.setPrototypeOf(Person,{
+//     name(){
+//         return 'vrjhbhfbvbh'
+//     }
+// })
+// const obj = {}
+// Object.defineProperty(obj, 'name', {
+//     configurable:true,
+//     writable: true,
+//     value: 'frerf'
+// })
+// console.log(Object.getOwnPropertyDescriptors(obj))
+
+const obj = {
+    name: '1',
+    age: 'age',
+    [Symbol()]: 'sy'
 }
-const person = new Person('111')
-const person2 = new Person('222')
-person.sayName()
-person2.sayName()
-console.log(person.sayName === person2.sayName,Person.sayName)
+obj.__proto__.other = 'yy'
+Object.defineProperty(obj, 'ko',{
+    enumerable: true,
+    value: 'okkok'
+})
+console.log(Object.getOwnPropertyNames(obj))
+
+
+
