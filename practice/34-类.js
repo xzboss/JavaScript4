9be0cb894baea74cd3cac82bef87f9d88fd3eba1
xzboss <942875315@qq.com>
2023-12-12 10:04:57 +0800

    fix: 🐛 类补充

--- practice/34-类.js
+++ practice/34-类.js
@@ -16,13 +16,31 @@
 // console.log(Person.say["HomePage"]);
 
 //
-class Person {
-  constructor() {
-    console.log(this.name);
+// class Person {
+//   constructor() {
+//     console.log(this.name);
 
-    console.log(this.say);
-  }
-  say() {}
-  name = "xz";
+//     console.log(this.say);
+//   }
+//   say() {}
+//   name = "xz";
+// }
+// const p = new Person();
+
+class Person {
+  ['w' + 'd'] = '';
+  [Symbol()] = '///';
+  [2 + 2] = '444';
+  a() {}
 }
-const p = new Person();
+class Boss extends Person {
+  constructor() {
+    super();
+    console.log(this.name, this.fun, this.foo, '@@@');
+  }
+  name = 'xz';
+  fun() {}
+  foo = function () {};
+}
+const boss = new Boss();
+console.log(boss);
